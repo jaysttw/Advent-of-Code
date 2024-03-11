d886110fac8c948e8e5bed461336b640d0545831
Jay <jayseetow@gmail.com>
2024-03-11 21:00:37 +0000

    2022 Day 6 part 2

--- 2022/AoC-Rust-2022/src/day6.rs
+++ 2022/AoC-Rust-2022/src/day6.rs
@@ -7,14 +7,23 @@ use std::collections::HashSet;
 //     input
 // }
 
-#[aoc(day6, part1, mine)]
-fn part1(input: &str) -> usize {
+fn find_distinct_characters(input: &str, n: usize) -> usize {
     let cs: Vec<char> = input.chars().collect();
 
     let truth_vec: Vec<bool> = cs
-    .windows(4)
-    .map(|x| HashSet::<&char>::from_iter(x.iter()).len() == 4)
+    .windows(n)
+    .map(|x| HashSet::<&char>::from_iter(x.iter()).len() == n)
     .collect();
 
-    truth_vec.iter().position(|&x| x == true).expect("No unique match found!") + 4
+    truth_vec.iter().position(|&x| x == true).expect("No unique match found!") + n
+}
+
+#[aoc(day6, part1, mine)]
+fn part1(input: &str) -> usize {
+    find_distinct_characters(input, 4)
+}
+
+#[aoc(day6, part2, mine)]
+fn part2(input: &str) -> usize {
+    find_distinct_characters(input, 14)
 }
